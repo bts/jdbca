80fa6d707465954ec3bac222969700303fa6bc5b
Brian Schroeder <bts@gmail.com>
2015-03-09 08:51:54 -0400

    Mark version 0.1.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject jdbca "0.1.0-SNAPSHOT"
+(defproject jdbca "0.1.0"
   :description "An asynchrous interface to java.jdbc."
   :url "http://github.com/bts/jdbca"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/java.jdbc "0.3.6"]
-                 [manifold "0.1.0-SNAPSHOT"]
+                 [manifold "0.1.0-beta11"]
                  [io.aleph/dirigiste "0.1.0-alpha8"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                                   [org.postgresql/postgresql "9.4-1201-jdbc41"]
